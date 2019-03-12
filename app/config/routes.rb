43580a4aec82f9f1c444394e7c697c97b02d6cd3
Cristian-Sainea <cdsaineac@unal.edu.co>
2019-03-12 03:27:54 +0000

    migration file

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
     
-     get 'huespedsservicios/edit', to: "huespedservicios#peppopig"
+    get 'huespedsservicios/index', to: "huespedservicios#index"
+    get 'huespedsservicios/edit', to: "huespedservicios#edit"
+    get 'huespedsservicios/show', to: "huespedservicios#show"
+    get 'huespedsservicios/new', to: "huespedservicios#new"
+    
     root 'pages#welcome'
   
 end
