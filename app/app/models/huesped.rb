e32cbd97e111b3c7587c0483eeecc50ceebd3dd8
Cristian-Sainea <cdsaineac@unal.edu.co>
2019-03-12 07:26:47 +0000

    uniqueness of primary key huespeds

--- app/app/models/huesped.rb
+++ app/app/models/huesped.rb
@@ -1,5 +1,6 @@
 class Huesped < ApplicationRecord #ActiveRecord::Base
     self.primary_key = 'cedula'
+    validates_uniqueness_of :cedula
     has_many :solicituds
     has_many :servicios, through: :solicituds
 end 
