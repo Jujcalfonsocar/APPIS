84621a67a762c2ad60e3461d2b3846b161d93711
Cristian-Sainea <cdsaineac@unal.edu.co>
2019-03-12 06:05:49 +0000

    cambio de nombre a la tabla

--- app/db/migrate/20190312054001_add_descripcionsolicitudes.rb
+++ app/db/migrate/20190312054001_add_descripcionsolicitudes.rb
@@ -1,8 +1,8 @@
 class AddDescripcionsolicitudes < ActiveRecord::Migration[5.0]
    def change
     
-        add_column :solicitudes, :precio, :int8
-        add_column :solicitudes, :especificaciones, :text
+        add_column :solicituds, :precio, :int8
+        add_column :solicituds, :especificaciones, :text
   
   end
 end
