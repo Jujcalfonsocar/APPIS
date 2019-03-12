36b0e58497910f49f7f28ef8ab89b1a97745e520
Cristian-Sainea <cdsaineac@unal.edu.co>
2019-03-12 03:21:28 +0000

    migration file

---
+++ app/db/migrate/20190312030729_create_huespeds.rb
@@ -0,0 +1,6 @@
+class CreateHuespeds < ActiveRecord::Migration[5.0]
+  def change
+    create_table :huespeds do |t|
+    end
+  end
+end
--- app/db/migrate/20190312031948_add_descripcionhuespeds.rb
+++ app/db/migrate/20190312031948_add_descripcionhuespeds.rb
@@ -1,6 +1,5 @@
 class AddDescripcionhuespeds < ActiveRecord::Migration[5.0]
   def change
-  
     add_column :huespeds, :cedula, :int8
     add_column :huespeds, :nombres, :text
     add_column :huespeds, :apellidos, :text
@@ -8,6 +7,5 @@ class AddDescripcionhuespeds < ActiveRecord::Migration[5.0]
     add_column :huespeds, :email, :text
     add_column :huespeds, :fecha_nacimiento, :date
     add_column :huespeds, :pais_origen, :text
-  
   end
 end
