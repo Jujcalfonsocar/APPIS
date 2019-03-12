03c91ae1e0c12f40f6d3dfc2a59059e9656406b0
Cristian-Sainea <cdsaineac@unal.edu.co>
2019-03-12 05:36:12 +0000

    cambiando a tabla solicitudes

--- app/db/migrate/20190312010312_add_descripcionsolicitudes.rb
+++ app/db/migrate/20190312010312_add_descripcionsolicitudes.rb
@@ -1,4 +1,4 @@
-class AddDescripcionjoinhuespedservicio < ActiveRecord::Migration[5.0]
+class AddDescripcionsolicitudes < ActiveRecord::Migration[5.0]
   def change
     
     add_column :solicitudes, :precio, :int8
